9736f47928f9b6050203139aa48565155c5dd3b5
minhquang1008 <115976900+minhquang1008@users.noreply.github.com>
2023-09-22 13:41:28 +0700

    Add files via upload

---
+++ searcher.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+class Data:
+
+    def __init__(self):
+        self.__table = None
+
+    @property
+    def table(self):
+        return self.__table
+
+    @table.setter
+    def table(self, value):
+        self.__table = value
+
+    def getData(self, column_name):
+        sheet_id = '1Rgst7ZPw2b9dj4YXwa24vHzLaLEq4vuv'
+        df = pd.read_excel(
+            f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx',
+            sheet_name=self.table,
+            skiprows=[0, 1]
+        )
+        df.dropna(axis=0, how='all', inplace=True)
+        df.dropna(axis=1, how='all', inplace=True)
+        return df[df['Target field'] == column_name].melt()
+        
+
