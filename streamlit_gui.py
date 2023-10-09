ff78ce00b5273b61f1cccf803b06c3cdd47795eb
minhquang1008 <115976900+minhquang1008@users.noreply.github.com>
2023-10-09 09:58:31 +0700

    Add files via upload

--- streamlit_gui.py
+++ streamlit_gui.py
@@ -67,7 +67,35 @@ returning_dictionary = {0: 'information',
                      58: 'information/dim - vendor/province',
                      59: 'information/dim - vendor/contract_number_normal',
                      60: 'information/dim - vendor/contract_number_margin',
-                     }
+                     64: 'Margin',
+                     67: 'Margin/fact - outstanding',
+                     68: 'Margin/fact - outstanding/Source ID',
+                     69: 'Margin/fact - outstanding/Date Key',
+                     70: 'Margin/fact - outstanding/Date Oustanding',
+                     71: 'Margin/fact - outstanding/Customer ID',
+                     72: 'Margin/fact - outstanding/Oustanding Principal',
+                     73: 'Margin/fact - outstanding/Oustanding Remain',
+                     74: 'Margin/fact - outstanding/Oustanding Overdue',
+                     75: 'Margin/fact - outstanding/Oustanding Paid',
+                     76: 'Margin/fact - outstanding/Interest OUT',
+                     77: 'Margin/fact - outstanding/Interest Due',
+                     78: 'Margin/fact - outstanding/Interest Overdue',
+                     79: 'Margin/fact - outstanding/Interest Overdue OUT',
+                     80: 'Margin/fact - outstanding/Interest Paid',
+                     81: 'Margin/fact - outstanding/Date Disbursement',
+                     82: 'Margin/fact - outstanding/Date Start Interest Paid',
+                     83: 'Margin/fact - outstanding/Date First Due',
+                     84: 'Margin/fact - outstanding/Date Last Due',
+                     85: 'Margin/fact - outstanding/Rate Due 1',
+                     86: 'Margin/fact - outstanding/Rate Due 2',
+                     87: 'Margin/fact - outstanding/Rate Overdue',
+                     88: 'Margin/fact - outstanding/Loan Name',
+                     89: 'Margin/fact - outstanding/Branch ID',
+                     90: 'Margin/fact - outstanding/.Interest Outs',
+                     91: 'Margin/fact - outstanding/.Interest Overdue',
+                     92: 'Margin/fact - outstanding/.Outs Overdue',
+                     93: 'Margin/fact - outstanding/.Outs Principal'
+                    }
 #st-emotion-cache
 # with open('returning_dictionary.pkl', 'wb') as file:
 #     pickle.dump(returning_dictionary, file)
@@ -98,7 +126,7 @@ border-style: double;
 border-color: #34693a;
 border-radius: 20px;
 }
-[class="block-container css-1y4p8pa ea3mdgi4"] {
+[class="block-container st-emotion-cache-1y4p8pa ea3mdgi4"] {
 background-color: #FFFFFF;
 padding-top: 5px;
 margin-top: 0px;
@@ -155,9 +183,11 @@ with st.sidebar:
             sac.TreeItem('Trading', disabled=True, icon='table'),
             sac.TreeItem('Price Board',  disabled=True, icon='table')
         ]),
-        sac.TreeItem('Margin', tooltip='item3 tooltip', disabled=True, children=[
+        sac.TreeItem('Margin', tooltip='item3 tooltip', children=[
             sac.TreeItem('Room', disabled=True, icon='table'),
-            sac.TreeItem('Margin Detail',  disabled=True, icon='table')
+            sac.TreeItem('Margin Detail',  disabled=True, icon='table'),
+            sac.TreeItem('Outstanding', icon='table', children=[sac.TreeItem(f'{i}', icon='arrow-return-right') for i in
+                                        [returning_dictionary.get(k).split('/')[-1] for k in range(68, 94, 1)]])
         ]),
     ],  label='Table',
         index=0,
@@ -197,3 +227,4 @@ if len(the_path.split('/')) == 3:
     if str(df['Formula'].iloc[0]) != 'nan':
         st.write('### Formula')
         st.write(str(df['Formula'].iloc[0]))
+
