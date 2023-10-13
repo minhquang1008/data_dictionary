28dba06f7533914e15de953b39e8f4b3ce9274ef
minhquang1008 <115976900+minhquang1008@users.noreply.github.com>
2023-10-13 11:12:22 +0700

    Add files via upload

--- streamlit_gui.py
+++ streamlit_gui.py
@@ -4,9 +4,7 @@ import pandas as pd
 import pickle
 from searcher import Data, get_updated
 import datetime as dt
-
-
-sheet_list = ['Dim - Branch', 'Dim - Broker', 'Dim - Company', 'Dim - Customer', 'Dim - Territory', 'Dim - Vendor', 'Fact - Trading', 'Fact - Price Board', 'Fact - Outstanding', 'Fact - Room', 'Fact - Margin Detail']
+sheet_list = ['Dim - Branch', 'Dim - Broker', 'Dim - Company', 'Dim - Customer', 'Dim - Territory', 'Dim - Vendor', 'Fact - Outstanding', 'Fact - Room', 'Fact - Margin Detail', 'Fact - Trading', 'Fact - Price Board',]
 information_table = ['Dim - Branch', 'Dim - Broker', 'Dim - Company', 'Dim - Customer', 'Dim - Territory', 'Dim - Vendor']
 margin_table = ['Fact - Outstanding', 'Fact - Room', 'Fact - Margin Detail']
 trading_table = ['Fact - Trading', 'Fact - Price Board']
@@ -17,6 +15,8 @@ with open('returning_dictionary.pkl', 'rb') as file:
     returning_dictionary = pickle.load(file)
 with open('last_update.pkl', 'rb') as file:
     last_update = pickle.load(file)
+with open('last_search.pkl', 'rb') as file:
+    last_search = pickle.load(file)
 
 st.title('DATA DICTIONARY')
 st.markdown('''
@@ -37,7 +37,7 @@ border-style: double;
 border-color: #34693a;
 border-radius: 20px;
 }
-[class="block-container st-emotion-cache-1y4p8pa ea3mdgi4"] {
+[class="block-container css-1y4p8pa ea3mdgi4"] {
 background-color: #FFFFFF;
 padding-top: 5px;
 margin-top: 0px;
@@ -60,8 +60,6 @@ background-repeat: no-repeat;
 }
 </style>
 ''', unsafe_allow_html=True)
-with open('last_search.pkl', 'rb') as file:
-    last_search = pickle.load(file)
 df_search = pd.DataFrame(list(returning_dictionary.items()), columns=['index', 'path'])
 text_search = st.selectbox("Search column's name", df_search['path'])
 if text_search != last_search:
@@ -81,18 +79,19 @@ with st.sidebar:
                                       if len(q.split('/')) == 3 and q.split('/')[1] == i]])
             for i in information_table
         ]),
-        sac.TreeItem('Trading', tooltip='item2 tooltip', children=[
-            sac.TreeItem(i.split('-')[-1].strip(), icon='table', children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
-                                     [q.split('/')[-1] for q in returning_dictionary.values()
-                                      if len(q.split('/')) == 3 and q.split('/')[1] == i]])
-            for i in trading_table
-        ]),
         sac.TreeItem('Margin', tooltip='item3 tooltip',  children=[
             sac.TreeItem(i.split('-')[-1].strip(), icon='table',
                          children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
                                    [q.split('/')[-1] for q in returning_dictionary.values()
                                     if len(q.split('/')) == 3 and q.split('/')[1] == i]])
             for i in margin_table
+        ]),
+        sac.TreeItem('Trading', tooltip='item2 tooltip', children=[
+            sac.TreeItem(i.split('-')[-1].strip(), icon='table',
+                         children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
+                                   [q.split('/')[-1] for q in returning_dictionary.values()
+                                    if len(q.split('/')) == 3 and q.split('/')[1] == i]])
+            for i in trading_table
         ]),
     ],  label='Table',
         index=0,
