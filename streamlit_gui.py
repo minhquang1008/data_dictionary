e57fa7915589c4dd9a66269a596751a6fb969239
minhquang1008 <115976900+minhquang1008@users.noreply.github.com>
2023-12-19 13:17:52 +0700

    Add files via upload

--- streamlit_gui.py
+++ streamlit_gui.py
@@ -80,95 +80,95 @@ background-repeat: no-repeat;
 </style>
 ''', unsafe_allow_html=True)
 IP = client_ip()
-# if IP == '116.118.113.140' or IP == '115.78.11.116':
-df_search = pd.DataFrame(list(returning_dictionary.items()), columns=['index', 'path'])
-text_search = st.selectbox("Search column's name", df_search['path'])
-st.title('DATA DICTIONARY')
-if text_search != last_search:
-    t2 = dt.datetime.now()
-    with open('last_search.pkl', 'wb') as file:
-        pickle.dump(text_search, file)
-else:
-    t2 = dt.datetime(1993, 4, 16)
-with st.sidebar:
-    st.image("logo.png", width=200)
-    with open('last_clicked.pkl', 'rb') as file:
-        last_clicked = pickle.load(file)
-    clicked = sac.tree(items=[
-        sac.TreeItem('Information', tooltip='item1 tooltip', children=[
-            sac.TreeItem(i.split('-')[-1].strip(), icon='table', children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
-                                     [q.split('/')[-1] for q in returning_dictionary.values()
-                                      if len(q.split('/')) == 3 and q.split('/')[1] == i]])
-            for i in information_table
-        ]),
-        sac.TreeItem('Margin', tooltip='item3 tooltip',  children=[
-            sac.TreeItem(i.split('-')[-1].strip(), icon='table',
-                         children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
-                                   [q.split('/')[-1] for q in returning_dictionary.values()
-                                    if len(q.split('/')) == 3 and q.split('/')[1] == i]])
-            for i in margin_table
-        ]),
-        sac.TreeItem('Trading', tooltip='item2 tooltip', children=[
-            sac.TreeItem(i.split('-')[-1].strip(), icon='table',
-                         children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
-                                   [q.split('/')[-1] for q in returning_dictionary.values()
-                                    if len(q.split('/')) == 3 and q.split('/')[1] == i]])
-            for i in trading_table
-        ]),
-    ],  label='Table',
-        index=0,
-        format_func='title',
-        icon='folder-fill',
-        height=None,
-        open_index=None,
-        open_all=False,
-        checkbox=False,
-        show_line=True,
-        checkbox_strict=False,
-        return_index=True)
-    if clicked != last_clicked:
-        t1 = dt.datetime.now()
-        with open('last_clicked.pkl', 'wb') as file:
-            pickle.dump(clicked, file)
+if IP == '116.118.113.140' or IP == '115.78.11.116':
+    df_search = pd.DataFrame(list(returning_dictionary.items()), columns=['index', 'path'])
+    text_search = st.selectbox("Search column's name", df_search['path'])
+    st.title('DATA DICTIONARY')
+    if text_search != last_search:
+        t2 = dt.datetime.now()
+        with open('last_search.pkl', 'wb') as file:
+            pickle.dump(text_search, file)
     else:
-        t1 = dt.datetime(1993, 4, 16)
-    st.write(f"The client ip is {IP}")
-    if st.button("Update"):
-        with st.spinner('Wait for it...'):
-            returning_dictionary = get_updated(sheet_list, information_table, margin_table, trading_table)
-            with open('returning_dictionary.pkl', 'wb') as file:
-                pickle.dump(returning_dictionary, file)
-            last_update = dt.datetime.now()
-            with open('last_update.pkl', 'wb') as file:
-                pickle.dump(last_update, file)
+        t2 = dt.datetime(1993, 4, 16)
+    with st.sidebar:
+        st.image("logo.png", width=200)
+        with open('last_clicked.pkl', 'rb') as file:
+            last_clicked = pickle.load(file)
+        clicked = sac.tree(items=[
+            sac.TreeItem('Information', tooltip='item1 tooltip', children=[
+                sac.TreeItem(i.split('-')[-1].strip(), icon='table', children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
+                                         [q.split('/')[-1] for q in returning_dictionary.values()
+                                          if len(q.split('/')) == 3 and q.split('/')[1] == i]])
+                for i in information_table
+            ]),
+            sac.TreeItem('Margin', tooltip='item3 tooltip',  children=[
+                sac.TreeItem(i.split('-')[-1].strip(), icon='table',
+                             children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
+                                       [q.split('/')[-1] for q in returning_dictionary.values()
+                                        if len(q.split('/')) == 3 and q.split('/')[1] == i]])
+                for i in margin_table
+            ]),
+            sac.TreeItem('Trading', tooltip='item2 tooltip', children=[
+                sac.TreeItem(i.split('-')[-1].strip(), icon='table',
+                             children=[sac.TreeItem(f'{k}', icon='arrow-return-right') for k in
+                                       [q.split('/')[-1] for q in returning_dictionary.values()
+                                        if len(q.split('/')) == 3 and q.split('/')[1] == i]])
+                for i in trading_table
+            ]),
+        ],  label='Table',
+            index=0,
+            format_func='title',
+            icon='folder-fill',
+            height=None,
+            open_index=None,
+            open_all=False,
+            checkbox=False,
+            show_line=True,
+            checkbox_strict=False,
+            return_index=True)
+        if clicked != last_clicked:
+            t1 = dt.datetime.now()
+            with open('last_clicked.pkl', 'wb') as file:
+                pickle.dump(clicked, file)
+        else:
+            t1 = dt.datetime(1993, 4, 16)
+        st.write(f"The client ip is {IP}")
+        if st.button("Update"):
+            with st.spinner('Wait for it...'):
+                returning_dictionary = get_updated(sheet_list, information_table, margin_table, trading_table)
+                with open('returning_dictionary.pkl', 'wb') as file:
+                    pickle.dump(returning_dictionary, file)
+                last_update = dt.datetime.now()
+                with open('last_update.pkl', 'wb') as file:
+                    pickle.dump(last_update, file)
 
-# ----------------------------------------------------------------------------
-data = Data()
-if clicked and text_search and t1 > t2:
-    the_path = returning_dictionary.get(clicked[0])
+    # ----------------------------------------------------------------------------
+    data = Data()
+    if clicked and text_search and t1 > t2:
+        the_path = returning_dictionary.get(clicked[0])
+    else:
+        the_path = text_search
+    st.text(the_path.title())
+    if len(the_path.split('/')) == 3:
+        data.table = the_path.split('/')[1].title()
+        df = data.getData(the_path.split('/')[-1])
+        st.write(f'''## {the_path.split('/')[-1].title()}''')
+        st.write('article - ' + str(df['article'].iloc[0].strftime("%d/%m/%Y")))
+        if str(df['Description'].iloc[0]) != 'nan':
+            st.write(str(df['Description'].iloc[0]))
+        st.write('### Managed by')
+        st.write(str(df['Managed by'].iloc[0]))
+        st.write('### Data source')
+        st.write(df[['Source', 'Table', 'Field']].style.hide(axis="index").to_html(), unsafe_allow_html=True)
+        if str(df['Formula'].iloc[0]) != 'nan':
+            st.write('### Formula')
+            st.write(str(df['Formula'].iloc[0]))
+    st.divider()
+    st.success(f'last update: {last_update.strftime("%m/%d/%Y, %H:%M:%S")}')
+elif IP is None:
+    st.write("### Wait for it")
 else:
-    the_path = text_search
-st.text(the_path.title())
-if len(the_path.split('/')) == 3:
-    data.table = the_path.split('/')[1].title()
-    df = data.getData(the_path.split('/')[-1])
-    st.write(f'''## {the_path.split('/')[-1].title()}''')
-    st.write('article - ' + str(df['article'].iloc[0].strftime("%d/%m/%Y")))
-    if str(df['Description'].iloc[0]) != 'nan':
-        st.write(str(df['Description'].iloc[0]))
-    st.write('### Managed by')
-    st.write(str(df['Managed by'].iloc[0]))
-    st.write('### Data source')
-    st.write(df[['Source', 'Table', 'Field']].style.hide(axis="index").to_html(), unsafe_allow_html=True)
-    if str(df['Formula'].iloc[0]) != 'nan':
-        st.write('### Formula')
-        st.write(str(df['Formula'].iloc[0]))
-st.divider()
-st.success(f'last update: {last_update.strftime("%m/%d/%Y, %H:%M:%S")}')
-# elif IP is None:
-#     st.write("### Wait for it")
-# else:
-#     st.write("### Access Denied! You are not allowed to access this page")
+    st.write("### Access Denied! You are not allowed to access this page")
 
 
 
